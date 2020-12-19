3ddc3e36ead3a835ca2833f3bf47b9d9da8e07c0
Volodymyr <volodymyrquo@gmail.com>
2020-12-19 18:06:20 +0200

    created folder config

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+
+const PORT = config.get('serverPort');
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('dbUrl'));
+    app.listen(PORT, () => {
+      console.log('Server started on port: ', PORT);
+    });
+  } catch (error) {}
+};
+
+start();
